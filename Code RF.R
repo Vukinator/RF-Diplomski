2d263586519e4d6db5e95ba895cb60d297b8b423
Vukinator <35816915+Vukinator@users.noreply.github.com>
2024-12-17 17:46:59 +0100

    Update Code RF.R

--- Code RF.R
+++ Code RF.R
@@ -1,26 +1,26 @@
-# Podaci
-library(rpart)
-library(rpart.plot)
-library(ggplot2)
-library(randomForest)
-install.packages("rfUtilities"); library(rfUtilities) # instalirati ručno
-
-sve.raw<- read.csv("ESS10.csv")
+ess.raw<- read.csv("ESS10.csv")
 bdp<- read.csv("BDP.csv")
 bdp<- bdp[-(32:40), -(2:4)]
 bdp<- bdp[-c(21:23,24:29), ]
 names(bdp)<- c("country", "income")
 
 bdp$country<- sort(bdp$country)
-sve<- sve.raw[, c("cntry", "stflife","anweight", "health",
-             "marsts", "domicil", "iplylfr", "rlgdgr",
-             "pray", "rlgatnd", "wkhtot", "eisced", 
-             "hinctnta","hincfel", "hhmmb")]
+ess<- ess.raw[, c("cntry", "stflife", "health", "hhmmb",
+                  "rshpsts","domicil", "sclmeet","iplylfr", 
+                  "inprdsc", "imptrad", "imprich","rlgdgr",
+                  "rlgdnm","pray", "rlgatnd", "wkhtot",
+                  "eisced","hinctnta","gndr","agea")]
 
-cntry<- as.factor(sve$cntry)
+# da stavim i spol (gndr) i dob (agea)? i important to be rich (imprich) i important to follow traditions and customs(imptrad) za zajednicu kod iplylfr?
+# da stavim i koliko se često nalaze s prijateljima (sclmeet) i s koliko može ljudi razgovarati o intimnim stvarima (inprdsc)
+# koji je odnos s onim s kojim živi (rshpsts)- to ide za obitelj
+cntry<- as.factor(ess$cntry)
 
-health<- sve$health
-health[health== 1]<- 10
+gndr<- as.factor(ess$gndr)
+age<- ess$agea
+
+health<- ess$health
+health[health== 1]<- 10 # zamjena; obrnuti redoslijed
 health[health== 5]<- 1 
 health[health== 10]<- 5
 health[health== 2]<- 9
@@ -29,31 +29,46 @@ health[health== 9]<- 4
 health<- as.integer(health)
 health[health> 5]<- floor(mean(health, na.rm= T))
 
-stflife<- sve$stflife
+stflife<- ess$stflife
 stflife[stflife>10]<- floor(mean(stflife, na.rm= T))
 stflife<- cut(stflife, 
-             breaks= c(-1, 2, 4, 6, 8, 10),
-             labels= c(1, 2, 3, 4, 5),
-             right= T)# to je to; automatski postaje factor
-# a možda može i 4 kategorije; našel sam opravdanje u knjizi
+              breaks= c(-1, 2, 4, 6, 8, 10),
+              labels= c(1, 2, 3, 4, 5),
+              right= T) # 5 KATEGORIJA; automatski postaje factor
+
 levels(stflife)<- c("Totally unsatisfied",
                     "Unsatisfied",
                     "Not satisfied nor unsatisfied",
                     "Satisfied",
                     "Totally satisfied")
 
-marsts<- sve$marsts
-table(marsts)
-marsts[marsts>6]<- 6 # mod je inače 66, ali to znači "Not Applicable", tak da sam uzel da je mod 6
-marsts<- as.factor(marsts)
-levels(marsts)<- c("Legally married",
-                   "In a legally registered union",
-                   "Legally separated",
-                   "Legally divorced/Civil union dissolved",
-                   "Widowed/Civil partner died",
-                   "None of these")
+#stflife<- ess$stflife
+#stflife[stflife>10]<- floor(mean(stflife, na.rm= T))
+#stflife<- cut(stflife, 
+#              breaks= c(-1, 2, 5, 8, 10),
+#             labels= c(1, 2, 3, 4),
+#              right= T) # 4 KATEGORIJA; automatski postaje faktor
 
-domicil<- sve$domicil
+# a možda može i 4 kategorije; našel sam opravdanje u knjizi
+#levels(stflife)<- c("Totally unsatisfied",
+#                    "Unsatisfied",
+#                    "Satisfied",
+#                    "Totally satisfied")
+
+rshpsts<- ess$rshpsts
+table(rshpsts)
+rshpsts[rshpsts> 66]<-  1 
+rshpsts<- as.factor(rshpsts)
+levels(rshpsts)<- c("Legally married",
+                   "In a legally registered civil union",
+                   "Living with my partner - not legally recognised",
+                   "Living with my partner - legally recognised",
+                   "Legally separated",
+                   "Legally divorced",
+                   "Not applicable")
+
+
+domicil<- ess$domicil
 table(domicil)
 domicil[domicil>5]<- which.max(table(domicil)) # 56 NA vrijednosti
 domicil<- as.factor(domicil)
@@ -63,22 +78,65 @@ levels(domicil)<- c("A big city",
                     "Country village",
                     "Farm or home in countryside")
 
-iplylfr= sve$iplylfr # isto kao i za health; obrnuti redoslijed i ostaviti kao integer; jednostavno napravim jednu dodatnu varijablu za svaku zamjenu!; smijem li to?
-iplylfr[iplylfr== 1]<- 10
+iplylfr= ess$iplylfr
+iplylfr[iplylfr== 1]<- 10 # znači, tu mijenjamo redoslijed
 iplylfr[iplylfr== 6]<- 1
-iplylfr[iplylfr== 10]<- 1
-iplylfr[iplylfr== 2]<- 9
+iplylfr[iplylfr== 10]<- 6
+iplylfr[iplylfr== 2]<- 11
 iplylfr[iplylfr== 5]<- 2
-iplylfr[iplylfr== 9]<- 5
-iplylfr[iplylfr== 3]<- 8
+iplylfr[iplylfr== 11]<- 5
+iplylfr[iplylfr== 3]<- 13
 iplylfr[iplylfr== 4]<- 3
-iplylfr[iplylfr== 8]<- 3
-iplylfr[iplylfr> 6]<- mean(iplylfr[iplylfr< 6]) # mod
+iplylfr[iplylfr== 13]<- 4
+iplylfr[iplylfr> 6]<- floor(mean(iplylfr[iplylfr< 6])) # tak sam se riješil NA
 
-rlgdgr<- sve$rlgdgr
+imprich<- ess$imprich
+table(imprich)
+imprich[imprich== 1]<- 10 # isto kao i kod iplylfr
+imprich[imprich== 6]<- 1
+imprich[imprich== 10]<- 6
+imprich[imprich== 2]<- 11
+imprich[imprich== 5]<- 2
+imprich[imprich== 11]<- 5
+imprich[imprich== 3]<- 13
+imprich[imprich== 4]<- 3
+imprich[imprich== 13]<- 4
+imprich[imprich> 6]<- floor(mean(imprich[imprich< 6]))
+
+imptrad<- ess$imptrad
+table(imptrad)
+imptrad[imptrad== 1]<- 10
+imptrad[imptrad== 6]<- 1
+imptrad[imptrad== 10]<- 6
+imptrad[imptrad== 2]<- 11
+imptrad[imptrad== 5]<- 2
+imptrad[imptrad== 11]<- 5
+imptrad[imptrad== 3]<- 13
+imptrad[imptrad== 4]<- 3
+imptrad[imptrad== 13]<- 4
+imptrad[imptrad> 6]<- floor(mean(imptrad[imptrad< 6]))
+
+sclmeet<- ess$sclmeet
+sort(table(sclmeet), decreasing= T)
+sclmeet[sclmeet> 7]<- 6
+levels(sclmeet)<- c("Never", "Less than once a month",
+                    "Once a month", "Several times a month",
+                    "Once a week", "Several times a week",
+                    "Every day")
+
+inprdsc<- ess$inprdsc
+sort(table(inprdsc), decreasing= T)
+inprdsc[inprdsc> 6]<- 2
+inprdsc<- as.factor(inprdsc)
+levels(inprdsc)<- c("None", "1", "2", "3", "4-6", "7-9", "10+")
+
+
+
+
+rlgdgr<- ess$rlgdgr
 rlgdgr[rlgdgr>10]<- as.integer(mean(rlgdgr[rlgdgr<= 10], na.rm= T))
 
-rlgatnd<- sve$rlgatnd
+rlgatnd<- ess$rlgatnd
 table(rlgatnd)
 rlgatnd[rlgatnd> 7]<- which.max(table(rlgatnd))
 rlgatnd<- as.factor(rlgatnd)
@@ -89,8 +147,18 @@ levels(rlgatnd)<- c("Every day",
                     "Only on special holy days",
                     "Less often",
                     "Never")
+rlgdnm<- ess$rlgdnm
+sort(table(rlgdnm), decreasing= T)
+rlgdnm[rlgdnm> 8]<- 66
+rlgdnm<- as.factor(rlgdnm)
+levels(rlgdnm)<- c("Roman Catholic", "Protestant",
+                   "Eastern Orthodox", "Other Christian Denomination",
+                   "Jewish", "Islam", "Eastern religions", 
+                   "Other Non-Christian Religions",
+                   "Not applicable")
 
-pray<- sve$pray
+
+pray<- ess$pray
 pray[pray> 7]<- which.max(table(pray))
 pray<- as.factor(pray)
 levels(pray)<- c("Every day",
@@ -102,10 +170,10 @@ levels(pray)<- c("Every day",
                  "Never")
 
 
-wkhtot<- sve$wkhtot
-wkhtot[wkhtot>= 140]<- mean(wkhtot[wkhtot<= 140], na.rm= T) # mean od svih koji imaju manje od 120 radnih sati tjedno
+wkhtot<- ess.raw$wkhtot
+wkhtot[wkhtot>= 140]<- mean(wkhtot[wkhtot<= 140], na.rm= T)# mean od svih koji imaju manje od 120 radnih sati tjedno
 
-eisced<- sve$eisced
+eisced<- ess$eisced
 eisced[eisced> 55]<- which.max(table(eisced))
 eisced[eisced== 55]<- 8
 eisced<- as.factor(eisced)
@@ -119,10 +187,11 @@ levels(eisced)<- c("Not possible to harmonise into ES-ISCED",
                    "Higher tertiary education, >= MA level",
                    "Other")
 
-hinctnta<- sve$hinctnta
-hinctnta[hinctnta> 10]<- floor(mean(hinctnta[hinctnta<= 10], na.rm= T)) # ovo bi možda trebalo za svaku državu zajedno
+hinctnta<- ess$hinctnta
+hinctnta[hinctnta> 10]<- floor(mean(hinctnta[hinctnta<= 10], na.rm= T)) # ovo bi možda trebalo za svaku državu zajedno; i staviti kao faktor!
+hinctnta<- as.factor(hinctnta)
 
-hhmmb<- sve$hhmmb # veće od 13 moram pretvoriti u NA
+hhmmb<- ess$hhmmb # veće od 13 moram pretvoriti u NA
 hhmmb[hhmmb> 13]<- floor(mean(hhmmb[hhmmb<=13], na.rm= T))
 
 each<- table(cntry)
@@ -134,48 +203,41 @@ income<- c(45609.0034936111, 10148.3423954435, 85897.7843338323, 22992.879383334
            25558.4290544506, 19552.0911095911)
 bdp.prihodi<- as.numeric(unlist(mapply(rep, income, each)))
 
-
 # JUHU HEHE
-sve<- data.frame(cntry, stflife, bdp.prihodi, health, hinctnta, iplylfr, hhmmb,
-                domicil, wkhtot, rlgdgr, rlgatnd, pray, marsts)
-
-# još napraviti primanja prema decilu kojemu svatko pripada u BDPu i to je to
-# TO JE DATASET
-
-
-# izračunati primanja po decilima; KONZULTACIJE!!!
-library(ggplot2)
-ggplot(data= sve, mapping= aes(x= hinctnta)) + geom_histogram(binwidth = 1) # jasno se vidi distrbucija
-
+ajmo<- data.frame(cntry, bdp.prihodi, stflife, health, hhmmb,
+                 rshpsts, domicil, sclmeet,iplylfr, 
+                 inprdsc, imptrad, imprich,rlgdgr,
+                 rlgdnm,pray, rlgatnd, wkhtot,
+                 eisced,hinctnta,gndr,age)
 
 # REGIJE
-be= subset(sve, cntry== "BE")
-hr= subset(sve, cntry== "HR")
-bg= subset(sve, cntry=="BG")
-ch= subset(sve, cntry=="CH")
-cz= subset(sve, cntry== "CZ")
-ee= subset(sve, cntry=="EE")
-fi= subset(sve, cntry=="FI")
-fr= subset(sve, cntry== "FR")
-gb= subset(sve, cntry== "GB")
-gr= subset(sve, cntry== "GR")
-hu= subset(sve, cntry=="HU")
-ie= subset(sve, cntry=="IE")
-is= subset(sve, cntry== "IS")
-it= subset(sve, cntry== "IT")
-lt= subset(sve, cntry== "LT")
-me= subset(sve, cntry== "ME")
-mk= subset(sve, cntry== "MK")
-nl= subset(sve, cntry=="NL")
-no= subset(sve, cntry== "NO")
-pt= subset(sve, cntry=="PT")
-sl= subset(sve, cntry=="SI")
-sk= subset(sve, cntry=="SK")
+be<- subset(ajmo, cntry== "BE")
+hr<- subset(ajmo, cntry== "HR")
+bg<- subset(ajmo, cntry=="BG")
+ch<- subset(ajmo, cntry=="CH")
+cz<- subset(ajmo, cntry== "CZ")
+ee<- subset(ajmo, cntry=="EE")
+fr<- subset(ajmo, cntry== "FR")
+fi<- subset(ajmo, cntry== "FI") 
+gb<- subset(ajmo, cntry== "GB")
+gr<- subset(ajmo, cntry== "GR")
+hu<- subset(ajmo, cntry=="HU")
+ie<- subset(ajmo, cntry=="IE")
+is<- subset(ajmo, cntry== "IS")
+it<- subset(ajmo, cntry== "IT")
+lt<- subset(ajmo, cntry== "LT")
+me<- subset(ajmo, cntry== "ME")
+mk<- subset(ajmo, cntry== "MK")
+nl<- subset(ajmo, cntry=="NL")
+no<- subset(ajmo, cntry== "NO")
+pt<- subset(ajmo, cntry=="PT")
+sl<- subset(ajmo, cntry=="SI")
+sk<- subset(ajmo, cntry=="SK")
 
-zapadna= na.roughfix(rbind(be, ch, fr, gb, ie, nl))
-sjeverna= na.roughfix(rbind(ee, fi, lt, is))
-juzna= na.roughfix(rbind(it, pt, gr))
-istocna= na.roughfix(rbind(hr, bg, cz, hu, me, mk, sl, sk))
+zapadna= rbind(be, ch, fr, gb, ie, nl)
+sjeverna= rbind(ee, fi, lt, is)
+juzna= rbind(it, pt, gr)
+istocna= rbind(hr, bg, cz, hu, me, mk, sl, sk)
 
 set.seed(57123)
 index.zapadna= sample(1:3, size= nrow(zapadna), replace= T, prob= c(0.7, 0.2, 0.1))
@@ -187,7 +249,7 @@ index.istocna= sample(1:3, nrow(istocna), replace= T, prob= c(0.7, 0.2, 0.1))
 trainset.istocna= istocna[index.istocna==1, ]
 testset.istocna = istocna[index.istocna== 2, ]
 valset.istocna= istocna[index.istocna== 3, ]
-  
+
 index.sjeverna= sample(1:3, nrow(sjeverna), replace= T, prob= c(0.7, 0.2, 0.1))
 trainset.sjeverna= sjeverna[index.sjeverna==1, ]
 testset.sjeverna = sjeverna[index.sjeverna== 2, ]
@@ -197,56 +259,3 @@ index.juzna= sample(1:3, nrow(juzna), replace= T, prob= c(0.7, 0.2, 0.1))
 trainset.juzna= juzna[index.juzna==1, ]
 testset.juzna= juzna[index.juzna== 2, ]
 valset.juzna= juzna[index.juzna== 3, ]
-
-# Primjer za rpart; treniranje i testiranje zapadne
-primjer_diplomski<- rpart(stflife~.,
-                          data= trainset.zapadna[, -1],
-                          cp= 0,
-                          minsplit= 2)
-plot(primjer_diplomski) # jao brate; ovo je ogromno
-primjer_cp<- primjer_diplomski$cptable
-
-primjer_cp[which.min(primjer_cp[, 4]), ]
-primjer_optimal<- prune(primjer_diplomski, cp= primjer_cp[which.min(primjer_cp[, 4])], 1)
-plot(primjer_optimal)
-text(primjer_optimal, xpd= T, cex= 0.5)
-rpart.plot(primjer_optimal,
-           type= 4,
-           extra= 101,
-           box.palette = "auto",
-           clip.facs= T) # da, mislim da imam problea s "underrepresented klasama"
-
-uvjet_1SE<- primjer_cp[which.min(primjer_cp[, 4]), 4:5]
-optimal_1SE= primjer_diplomski$cptable[min(which(primjer_diplomski$cptable[, 4] <= sum(uvjet_1SE))), 1]
-
-primjer_1SE<- prune(primjer_diplomski, cp= optimal_1SE)
-rpart.plot(primjer_1SE, 
-           type= 4, 
-           extra= 101, 
-           box.palette= "auto")
-rpart.rules(primjer_1SE, 
-            cover= T,
-            extra= 4)
-
-var
-
-### Zapadna
-sve= randomForest::na.roughfix(sve)
-set.seed(123)
-rftz= randomForest::randomForest(formula= stflife~., 
-                                 data= na.roughfix(trainset.zapadna[, -1]),
-                                 mtry= sqrt(ncol(sve)),
-                                 ntree= 500,
-                                 proximity= T,
-                                 do.trace= 25) # svakih 25 stabala da pokaže OOB i ostale pokazatelje)
-
-trainset.zapadna= trainset.zapadna[, -c(1, 3, 5)]
-
-brojmtry= as.numeric(vector(length= 8))
-najmtry= for (i in 1:8){
-  rfztrain= randomForest(stflife~., 
-                         data= na.roughfix(trainset.zapadna),
-                         mtry= i,
-                         ntree= 500)
-  brojmtry[i]= rfztrain$err.rate[nrow(rfztrain$err.rate), 1]
-}
